7105912bdda08d589762d1bf6ec9d1dad17392a4
faubi <faubiguy0@gmail.com>
2017-10-01 09:09:01 -0500

    Miscellaneous additions

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # HackUTA-f2017
 
-This project streams video recorded from a Kinect device to a web browser, after using facial recognition to detect people in the video.
+Visual motion tracking for disaster relief efforts. The KinectCam has 2 purposes: 1. security tracking for supply rooms, and, 2. using face recognition to detect persons in a disaster area for rescuing misssions
 
-Dependencies: gcc, python 2.7, opencv, libfreenect, libhdf5, ffmpeg, apache web server
+Dependencies: gcc, python 2.7, opencv, libfreenect, libhdf5, ffmpeg, apache web server, python2-pillow
 Web client includes code from (hls.js)[https://github.com/video-dev/hls.js]
+
+How to install
+--
+
+* Install the dependencies above
+* Clone this repository into a new directory.
+* Run `build.sh` to build the video capture tool.
+* Copy and edit `email.cfg` to set up email notifications
+* Configure the apache httpd server to serve the `http` directory
+
+How to run
+--
+
+* Connect a Kinect device to usb
+* Run `run.sh` to start the video transcoding
+
--- kinect_test2.py
+++ kinect_test2.py
@@ -19,6 +19,7 @@ faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 in_file = args.in_file
 last_image = 0;
 last_num_faces = 0
+firstFrame = None
 """
 Grabs a depth map from the Kinect sensor and creates an image from it.
 """
@@ -29,6 +30,24 @@ while 1:
         rgb.shape = (480, 640, 3)
         # sys.stderr.write(repr(rgb))
 	gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+	if firstFrame is None:
+		firstFrame = cv2.GaussianBlur(gray, (21, 21), 0)
+		continue
+	frameDelta = cv2.absdiff(firstFrame, gray)
+	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+	thresh = cv2.dilate(thresh, None, iterations=2)
+	_,cnts,hierarchy = cv2.findContours(thresh.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+        text = "Unoccupied"
+	for c in cnts:
+		# if the contour is too small, ignore it
+		if cv2.contourArea(c) < 1000:
+			continue
+ 
+		# compute the bounding box for the contour, draw it on the frame,
+		# and update the text
+		(x, y, w, h) = cv2.boundingRect(c)
+		# cv2.rectangle(rgb, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		text = "Occupied"
 
 	faces = faceCascade.detectMultiScale(
     		gray,
@@ -37,23 +56,27 @@ while 1:
     		minSize=(30, 30),
     		flags = cv2.CASCADE_SCALE_IMAGE
 		)
-	# print "Found {0} faces!".format(len(faces))
+
+        now = time.time()
 
 	# Draw a rectangle around the faces
 	for (x, y, w, h) in faces:
     		cv2.rectangle(rgb, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+	cv2.putText(rgb, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+	cv2.putText(rgb, time.strftime("%A %d %B %Y %I:%M:%S%p",time.gmtime(now)), (10, rgb.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
         rgb.tofile(sys.stdout)
 
-        if faces:
-            now = time.time();
+        if len(faces) > 0:
             if now >= last_image + 60 or len(faces) > last_num_faces:
                 last_image = now
                 last_num_faces = len(faces)
-                image = Image.fromarray(rgb, now)
+                image = Image.fromarray(rgb)
                 formatted_time = time.strftime('%Y-%m-%d-%H:%M:%S',time.gmtime(now))
                 if not os.path.isdir('http/gallery'):
                     os.mkdir('http/gallery')
                 image_name = 'http/gallery/{0}.png'.format(formatted_time)
                 image.save(image_name)
 
-                subprocess.Popen(["python2", "send_email.py", str(num_faces), image_name, formatted_time]);
+                subprocess.Popen(["python2", "send_email.py", str(len(faces)), image_name, formatted_time]);
